87c3f9ffdb495b50d0004ddafbff844f105f99b8
Ayush Acharjya <ayushacharjya@gmail.com>
2024-04-15 20:30:14 +0100

    univariate linear reg done

---
+++ Supervised Learning/Linear Regression/Linear Regression.py
@@ -0,0 +1,89 @@
+# simple univariate linear regression
+# author : Ayush
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+# calculate arithmetic mean avg
+def arith_mean(arr):
+    return np.mean(arr)
+
+
+def squared_error(original, model):
+    return sum((model - original) ** 2)
+
+
+# calculate coefficient of determination R^2
+def coff_of_deter(original, model):
+    mean_line = [arith_mean(original) for _ in original]
+    sq_error = squared_error(original, model)
+    sq_error_am = squared_error(original, mean_line)
+    return 1 - (sq_error / sq_error_am)
+
+
+class LinearRegression:
+
+    def __init__(self):
+        self.intercept = 0
+        self.slope = 0
+
+    def best_fit(self, dim_one, dim_two):
+        self.slope = ((arith_mean(dim_one) * arith_mean(dim_two)) - arith_mean(dim_one * dim_two)) / (
+                arith_mean(dim_one) ** 2 - arith_mean(dim_one ** 2))
+        return self.slope
+
+    def y_inter(self, dim_one, dim_two):
+        self.intercept = arith_mean(dim_two) - (self.slope * arith_mean(dim_one))
+        return self.intercept
+
+    def predict(self, x):
+        return [(self.slope * param) + self.intercept for param in x]
+
+
+def main():
+    # read the dataset
+    file_path = 'Linear Regression - Sheet1.csv'
+    data = pd.read_csv(file_path)
+
+    # preprocess the data
+    X = np.array(data['X'])  # Assuming 'X' is the independent variable
+    y = np.array(data['Y'])  # Assuming 'Y' is the dependent variable
+
+    # Reshape X to a 2D array if needed
+    X = X.reshape(-1, 1)
+
+    # Split the dataset into training and testing sets test size = 0.01%
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
+
+    # Train the model
+    reg = LinearRegression()
+    slope = reg.best_fit(X_train.flatten(), y_train)
+    intercept = reg.y_inter(X_train.flatten(), y_train)
+
+    # predict on data set
+    y_pred = reg.predict(X_test.flatten())
+
+    # calculate R^2
+    r_sq = coff_of_deter(y_test, y_pred)
+    print("R^2 value : ", r_sq)
+
+    # mean squared error
+    mean_sq_err = np.mean((y_test - y_pred) ** 2)
+    print("Mean squared error is  : ", mean_sq_err)
+
+    # plot res
+    plt.scatter(X_train, y_train, color='blue', label='Training data')
+    plt.scatter(X_test, y_test, color='white', label='Testing data')
+    plt.plot(X_train, slope * X_train + intercept, color='red', label='Regression line')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Linear Regression')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
